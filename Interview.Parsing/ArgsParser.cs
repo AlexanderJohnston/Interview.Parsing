d0e7baeebe3ba8276b58f4059f64f1d7c2c03a54
Alexander Johnston <alexanderj.roaming@gmail.com>
2020-10-28 20:05:53 -0400

    Create a basic console application.
    Create the solution file with a single .NET Core 3.1 Console application with a basic command line argument parsing solution to accept both file and string inputs.

---
+++ Interview.Parsing/ArgsParser.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Interview.Parsing
+{
+    public class ArgsParser
+    {
+        /// <summary>
+        ///     Using a stack lets us keep track of the order they
+        /// </summary>
+        public List<string> FilesPassedAsArgs = new List<string>();
+
+        public List<string> Inputs = new List<string>();
+
+        public string InputFileName;
+        public string OutputFileName;
+
+        public ArgsParser(string[] args)
+        {
+            // Boundary checking before testing the first two arguments
+            bool inputFileProvided = false;
+            for (int i = 0; i < args.Length && i < 2; i++)
+            {
+                if (File.Exists(args[i]))
+                {
+                    // If the first arg is a file path we will assume it is the input
+                    inputFileProvided = i == 0 ? true : inputFileProvided;
+                    FilesPassedAsArgs.Add(args[i]);
+                }
+            }
+            if (inputFileProvided)
+            {
+                InputFileName = FilesPassedAsArgs[0];
+                OutputFileName = FilesPassedAsArgs.Count > 1 ? FilesPassedAsArgs[1] : string.Empty;
+                ReadInputFile();
+            }
+            // If there are no files passed in then we must assume the args are the input
+            else
+            {
+                if (args[0] == "-multi")
+                {
+                    ReadAsMultipleInputs(args);
+                }
+                else
+                {
+                    ReadAsSingularInput(args);
+                }
+            }
+        }
+
+        private void ReadInputFile()
+        {
+            if (InputFileName == null) return;
+            var lines = File.ReadAllLines(InputFileName);
+            Inputs.AddRange(lines);
+        }
+
+        private void ReadAsSingularInput(string[] args)
+        {
+            var input = string.Join(" ", args);
+            Inputs.Add(input);
+        }
+
+        private void ReadAsMultipleInputs(string[] args)
+        {
+            // Drop the -multi switch used to declare this mode.
+            var inputs = args.Skip(1).ToArray();
+            Inputs.AddRange(inputs);
+        }
+    }
+}
---
+++ Interview.Parsing/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace Interview.Parsing
+{
+    class Program
+    {
+        internal static string[] _args;
+
+        static void Main(string[] args)
+        {
+            _args = args;
+            if (args.Length == 0)
+            {
+                DisplayHelp();
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                FilePathSanityCheck();
+            }
+
+            var parsedArguments = new ArgsParser(args);
+
+
+        }
+
+        /// <summary>
+        /// Displays a help prompt on the console after checing if you have whitespace in any file paths not wrapped in quotes.
+        /// 
+        /// </summary>
+        private static void FilePathSanityCheck()
+        {
+            var windowsPathRegex = new Regex(@"^\w:\\.+?\w+\s(?:$|\w+|(?:[\w|()]+)?\\)");
+            var rawArguments = string.Join("", _args);
+            var match = windowsPathRegex.Match(rawArguments);
+            if (match.Success)
+            {
+                DisplayPathWarning();
+            }
+        }
+
+        private static void DisplayPathWarning()
+        {
+
+        }
+
+        private static void DisplayHelp()
+        {
+
+        }
+    }
+}
